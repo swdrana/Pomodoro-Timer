a37e348cb3ffd31e792499b841c1b3aa5f1fe950
swdrana <tctr2s@gmail.com>
2022-06-08 09:09:55 +0600

    audio added whit fixed some of bug, also added set custom pomodoro time

---
+++ src/pomodoro/timer/MusicStuff.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pomodoro.timer;
+
+import java.io.File;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+
+/**
+ *
+ * @author masudrana
+ */
+public class MusicStuff {
+    void playMusic(String musicLocation){
+        try {
+            File musicPath = new File(musicLocation);
+            if (musicPath.exists()) {
+                AudioInputStream audioInput = AudioSystem.getAudioInputStream(musicPath);
+                Clip clip = AudioSystem.getClip();
+                clip.open(audioInput);
+                clip.start();
+                clip.loop(0);
+            }else{
+                System.out.println("Music Not Found!");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
